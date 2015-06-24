a83e64f852ca78832dcd7c04d62a9dba08a4885e
Testelin Geoffrey <geoffrey.testelin@gmail.com>
2015-06-24 09:48:52 +0200

    Create Couleurs.h

---
+++ Couleurs.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Couleurs
+{
+public:
+
+    // Constructeur
+    Couleurs();
+
+    static void bleu();
+    static void vert();
+    static void turquoise();
+    static void rouge();
+    static void jaune();
+    static void blanc();
+    static void gris();
+    static void rose();
+
+    // Destructeur
+    virtual ~Couleurs();
+};
+
